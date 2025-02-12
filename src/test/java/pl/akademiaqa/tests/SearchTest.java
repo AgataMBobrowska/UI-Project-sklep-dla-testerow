832d2d648cf60e1e6c0df1cc6ec3008f3a2db68b
Agata Bobrowska <agata.m.bobrowska@gmail.com>
2025-02-12 14:32:58 +0100

    Parametrized test update

--- src/test/java/pl/akademiaqa/tests/SearchTest.java
+++ src/test/java/pl/akademiaqa/tests/SearchTest.java
@@ -6,9 +6,15 @@ import org.assertj.core.api.MatcherAssert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import pl.akademiaqa.pages.HomePage;
 import pl.akademiaqa.pages.SearchResultPage;
+
+import java.util.stream.Stream;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SearchTest extends BaseTest {
@@ -40,10 +46,25 @@ public class SearchTest extends BaseTest {
         assertThat(searchResultPage.getSearchResultSection().getProducts().size()).isEqualTo(1);
     }
 
+//    @ParameterizedTest
+//    @ValueSource(strings = {"mug", "t-shirt", "frame"})
+//    void should_return_products_by_product_name(String productName) {
+//        SearchResultPage searchResultPage = homePage.getTopMenuSearchSection().searchForProducts(productName);
+//        assertThat(searchResultPage.getSearchResultSection().getProducts().size()).isGreaterThan(0);
+//    }
+
     @ParameterizedTest
-    @ValueSource(strings = {"mug", "t-shirt", "frame"})
-    void should_return_products_by_product_name(String productName) {
+    @MethodSource("searchData")
+    void should_return_products_by_product_name(String productName, int productCounter) {
         SearchResultPage searchResultPage = homePage.getTopMenuSearchSection().searchForProducts(productName);
-        assertThat(searchResultPage.getSearchResultSection().getProducts().size()).isGreaterThan(0);
+        assertThat(searchResultPage.getSearchResultSection().getProducts().size()).isEqualTo(productCounter);
+    }
+
+    private static Stream<Arguments> searchData() {
+        return Stream.of(
+                Arguments.of("t-shirt", 1),
+                Arguments.of("mug", 5),
+                Arguments.of("frame", 4)
+        );
     }
 }
