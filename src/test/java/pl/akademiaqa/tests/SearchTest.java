95dca9548736533d52801a4204fb983c130676df
Agata Bobrowska <agata.m.bobrowska@gmail.com>
2025-02-12 14:22:43 +0100

    Udate: Parametrized Test

--- src/test/java/pl/akademiaqa/tests/SearchTest.java
+++ src/test/java/pl/akademiaqa/tests/SearchTest.java
@@ -5,6 +5,8 @@ import org.assertj.core.api.Assertions;
 import org.assertj.core.api.MatcherAssert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import pl.akademiaqa.pages.HomePage;
 import pl.akademiaqa.pages.SearchResultPage;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -17,13 +19,31 @@ public class SearchTest extends BaseTest {
     void beforeEach() {
 
         homePage = new HomePage(page);
+        page.navigate("https://skleptestera.pl/index.php");
     }
 
     @Test
     void should_return_products_by_search_name_mug() {
-
-        page.navigate("https://skleptestera.pl/index.php");
         SearchResultPage searchResultPage = homePage.getTopMenuSearchSection().searchForProducts("mug");
         assertThat(searchResultPage.getSearchResultSection().getProducts().size()).isEqualTo(5);
+    }
+
+    @Test
+    void should_return_products_by_search_name_frame() {
+        SearchResultPage searchResultPage = homePage.getTopMenuSearchSection().searchForProducts("frame");
+        assertThat(searchResultPage.getSearchResultSection().getProducts().size()).isEqualTo(4);
+    }
+
+    @Test
+    void should_return_products_by_search_name_tshirt() {
+        SearchResultPage searchResultPage = homePage.getTopMenuSearchSection().searchForProducts("t-shirt");
+        assertThat(searchResultPage.getSearchResultSection().getProducts().size()).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"mug", "t-shirt", "frame"})
+    void should_return_products_by_product_name(String productName) {
+        SearchResultPage searchResultPage = homePage.getTopMenuSearchSection().searchForProducts(productName);
+        assertThat(searchResultPage.getSearchResultSection().getProducts().size()).isGreaterThan(0);
     }
 }
